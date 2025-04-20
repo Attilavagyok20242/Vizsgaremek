1ffa2f52783c72f292b45a8d3e873ba0dfb480b2
Szlej09 <ricsigyula6@gmail.com>
2025-04-20 08:31:59 +0200

    Bugfix

--- oldalak/News.php
+++ oldalak/News.php
@@ -3,31 +3,39 @@
 $sql="SELECT id,hir_szoveg, hir_cim,datum FROM hirek ORDER BY datum DESC LIMIT 3";
 $result=$conn->query($sql);
 
-while($row=$result->fetch_assoc())
-{
-    $tomb[]=array
-    (
-        "id" => $row["id"],
-        "hir_szoveg" => $row["hir_szoveg"],
-        "hir_cim" => $row["hir_cim"],
-        "datum" => $row["datum"]
-    );
+if (mysqli_num_rows($result)>0) {
+
+    while($row=$result->fetch_assoc())
+    {
+    
+        $tomb[]=array
+        (
+            "id" => $row["id"],
+            "hir_szoveg" => $row["hir_szoveg"],
+            "hir_cim" => $row["hir_cim"],
+            "datum" => $row["datum"]
+        );
+    }
+    $elso_id=$tomb[0]["id"];
+    $elso_hir=$tomb[0]["hir_szoveg"];
+    $elso_hir_cim=$tomb[0]["hir_cim"];
+    $elso_datum=$tomb[0]["datum"];
+
+    $masodik_id=$tomb[1]['id'];
+    $masodik_hir=$tomb[1]["hir_szoveg"];
+    $masodik_hir_cim=$tomb[1]["hir_cim"];
+    $masodik_datum=$tomb[1]["datum"];
+
+    $harmadik_id=$tomb[2]['id'];
+    $harmadik_hir=$tomb[2]["hir_szoveg"];
+    $harmadik_hir_cim=$tomb[2]["hir_cim"];
+    $harmadik_datum=$tomb[2]["datum"];
 }
-$elso_id=$tomb[0]["id"];
-$elso_hir=$tomb[0]["hir_szoveg"];
-$elso_hir_cim=$tomb[0]["hir_cim"];
-$elso_datum=$tomb[0]["datum"];
-
-$masodik_id=$tomb[1]['id'];
-$masodik_hir=$tomb[1]["hir_szoveg"];
-$masodik_hir_cim=$tomb[1]["hir_cim"];
-$masodik_datum=$tomb[1]["datum"];
-
-$harmadik_id=$tomb[2]['id'];
-$harmadik_hir=$tomb[2]["hir_szoveg"];
-$harmadik_hir_cim=$tomb[2]["hir_cim"];
-$harmadik_datum=$tomb[2]["datum"];
-
+else{
+    $elso_id=0;
+    $masodik_id=0;
+    $harmadik_id=0;
+}
 
 ?>
 
@@ -54,12 +62,12 @@ $harmadik_datum=$tomb[2]["datum"];
             </div>
             <div class="scroll" id="scroll1">
                 <div class="szovegkeret">
-                    <h1 class="cim"><?php print $elso_hir_cim?></h1>
-                    <p class="szoveg"><?php print $elso_hir?></p>
-                    <p id="datum"><?php print $elso_datum?></p>
+                    <h1 class="cim"><?php if(isset($elso_hir_cim)){print $elso_hir_cim;}?></h1>
+                    <p class="szoveg"><?php if(isset($elso_hir)){print $elso_hir;}?></p>
+                    <p id="datum"><?php if(isset($elso_datum)){print $elso_datum;}?></p>
                     <img class="stamp" src="NewsImg/stamp.png">
                 </div>
-                <?php if(isset($_SESSION["id"])){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$elso_id.")'>";}?>  
+                <?php if(isset($_SESSION["id"])&& $elso_id!=0){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$elso_id.")'>";}?>  
             </div>
         </div>
         <div class="belsokeret">
@@ -67,11 +75,11 @@ $harmadik_datum=$tomb[2]["datum"];
                 <img src="NewsImg/rolledupscroll2.png" class="rolledkep"></div>
             <div class="scroll" id="scroll2">
                 <div class="szovegkeret">
-                <h1 class="cim"><?php print $masodik_hir_cim?></h1>
-                <p class="szoveg"><?php print $masodik_hir?></p>
-                <p id="datum"><?php print $masodik_datum?></p><img class="stamp" src="../NewsImg/stamp.png">
+                <h1 class="cim"><?php if(isset($masodik_hir_cim)){print $masodik_hir_cim;}?></h1>
+                <p class="szoveg"><?php if(isset($masodik_hir)){print $masodik_hir;}?></p>
+                <p id="datum"><?php if(isset($masodik_datum)){print $masodik_datum;}?></p><img class="stamp" src="../NewsImg/stamp.png">
                 </div>
-                <?php if(isset($_SESSION["id"])){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$masodik_id.")'>";}?>   
+                <?php if(isset($_SESSION["id"])&&$masodik_id!=0){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$masodik_id.")'>";}?>   
             </div>
         </div>
         <div class="belsokeret">
@@ -79,10 +87,10 @@ $harmadik_datum=$tomb[2]["datum"];
                 <img src="NewsImg/rolledupscroll2.png" class="rolledkep"></div>
             <div class="scroll" id="scroll3">
             <div class="szovegkeret">
-                <h1 class="cim"><?php print $harmadik_hir_cim?></h1>
-                <p class="szoveg"><?php print $harmadik_hir?></p>
-                <p id="datum"><?php print $harmadik_datum?></p><img class="stamp" src="../NewsImg/stamp.png"></div>
-                <?php if(isset($_SESSION["id"])){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$harmadik_id.")'>";}?> 
+                <h1 class="cim"><?php if(isset($harmadik_hir_cim)){print $harmadik_hir_cim;}?></h1>
+                <p class="szoveg"><?php if(isset($harmadik_hir)){print $harmadik_hir;}?></p>
+                <p id="datum"><?php if(isset($harmadik_datum)){print $harmadik_datum;}?></p><img class="stamp" src="../NewsImg/stamp.png"></div>
+                <?php if(isset($_SESSION["id"])&&$masodik_id!=0){print"<img src='NewsImg/Comment2.png' id='kommentgomb' class='komment' onclick='KommentSec(".$harmadik_id.")'>";}?> 
         </div> 
     </div>
     
